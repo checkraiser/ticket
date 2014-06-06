b840eb4ceec464242c4f4dc4f5ae299feb5e1d3c
h_dung <h_dung@brycen.com.vn>
2014-06-06 10:54:59 +0700

    You can do what you want with project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
 	def index
 		@projects = Project.all
 	end
@@ -20,15 +20,15 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
+		
 	end
 
 	def edit
-		@project = Project.find(params[:id])
+		
 	end
 
 	def update
-		@project = Project.find(params[:id])
+		
 		if @project.update(project_params)
 			flash[:notice] = "Project has been updated."
 			redirect_to @project
@@ -38,7 +38,7 @@ class ProjectsController < ApplicationController
 		end
 	end
 	def destroy
-		@project = Project.find(params[:id])
+		
 		@project.destroy
 		flash[:notice] = "Project has been destroyed."
 		redirect_to projects_path
@@ -46,5 +46,12 @@ class ProjectsController < ApplicationController
 	private
 	def project_params
 		params.require(:project).permit(:name, :description)
+	end
+	private
+	def set_project
+		@project = Project.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
 	end
 end

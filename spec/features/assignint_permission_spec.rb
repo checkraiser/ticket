949ee93686fee9f3ffe4b6cab362eb9a080f9467
h_dung <h_dung@brycen.com.vn>
2014-06-09 15:47:55 +0700

    Add creating, editing, updating and deleting tickets to assigning permissions interface

--- spec/features/assignint_permission_spec.rb
+++ spec/features/assignint_permission_spec.rb
@@ -21,4 +21,52 @@ feature 'Assigning permissions' do
 		sign_in_as!(user)
 		expect(page).to have_content(project.name)
 	end
+
+	scenario "Creating tickets for a project" do 
+		check_permission_box "view", project
+		check_permission_box "create_tickets", project
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link "New Mticket"
+		fill_in "Title", with: "Shinny!"
+		fill_in "Description", with: "Make it so!"
+		click_button "Create"
+
+		expect(page).to have_content("Ticket has been created.")
+	end
+
+	scenario "Updating tickets for a project" do 		
+		check_permission_box "view", project
+		check_permission_box "edit_tickets", project
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link mticket.title
+		click_link "Edit Ticket"
+
+		fill_in "Title", with: "Really Shinny!"		
+		click_button "Update"
+
+		expect(page).to have_content("Ticket has been updated.")
+	end
+
+	scenario "Deleting tickets for a project" do 		
+		check_permission_box "view", project
+		check_permission_box "delete_tickets", project
+		click_button "Update"
+		click_link "Sign out"
+
+		sign_in_as!(user)
+		click_link project.name
+		click_link mticket.title
+		click_link "Delete Ticket"
+		
+
+		expect(page).to have_content("Ticket has been deleted.")
+	end
 end
